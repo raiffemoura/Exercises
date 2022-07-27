97848d967da38f783519584840d3437d030e564c
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2022-07-27 16:55:19 +0100

    Js-notes

---
+++ JS-notes/script.js
@@ -0,0 +1,8 @@
+let countP = window.document.getElementsByTagName("p")
+console.log(countP)
+console.log(window.document.getElementsByTagName("p").length)
+
+var cars = ["jetta", "fusion", "corolla", "civic"]
+console.log(cars.length)
+
+console.log(countP)
