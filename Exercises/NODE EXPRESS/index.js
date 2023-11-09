de9c4b1311165d9f624e4e6a31351ca6f9c065ad
Raiffe Moura <109031102+raiffemoura@users.noreply.github.com>
2023-11-08 23:11:43 -0300

    node express

--- Exercises/NODE EXPRESS/index.js
+++ Exercises/NODE EXPRESS/index.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.use("/meusite", express.static(path.join(__dirname, "client")));
+
+app.get("/", (req, res) => {
+  res.type("html");
+  res.send("<h1>Hello World From GET</h1>");
+});
+
+app.post("/", (req, res) => {
+  res.send("<h1>Hello World From POST</h1>");
+});
+
+app.put("/", (req, res) => {
+  res.send("<h1>Hello World From PUT</h1>");
+});
+
+app.delete("/", (req, res) => {
+  res.send("<h1>Hello World From DELETE</h1>");
+});
 
 const PORT = 5000;
 app.listen(PORT, () => {
